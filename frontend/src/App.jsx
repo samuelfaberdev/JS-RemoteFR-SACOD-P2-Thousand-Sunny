de201b42791c69bfbfe197bc9557b1317c00f1b6
francoiscrtz <francoiscreutzer@gmail.com>
2023-10-26 16:57:55 +0200

    1ère vr footer done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App font-amarante">
       <h1 className="font-amarante font-bold text-4xl">
         Whereas disregard and contempt for human rights have resulted
       </h1>
-    </div>
-  );
+      <Footer/>
+      </div>
+       );
+
+  
 }
 
 export default App;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className = "bg-back fixed bottom-0 p-4 w-screen text-center font-bold">
+        <h1>© Thousand Sunny 2023</h1>
+    </div>
+  );
+}
+
+export default Footer;
