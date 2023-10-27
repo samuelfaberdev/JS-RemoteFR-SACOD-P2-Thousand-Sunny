16ddff8021433852ddeea0a1218c906ea6070421
francoiscrtz <francoiscreutzer@gmail.com>
2023-10-27 09:24:23 +0200

    fixed prettier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,9 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App font-amarante">
-      <h1 className="font-amarante font-bold text-4xl">
-        Whereas disregard and contempt for human rights have resulted
-      </h1>
-      <Footer/>
-      </div>
-       );
-
-  
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Footer = () => {
+function Footer() {
   return (
-    <div className = "bg-back fixed bottom-0 p-4 w-screen text-center font-bold">
-        <h1>© Thousand Sunny 2023</h1>
+    <div className="bg-back fixed bottom-0 p-4 w-screen text-center font-bold">
+      <h1>© Thousand Sunny 2023</h1>
     </div>
   );
 }
