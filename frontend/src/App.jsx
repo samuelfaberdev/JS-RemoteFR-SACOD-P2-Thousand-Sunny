437dbce554943780592b19b0b82030dde5f83463
Mehdi Mehemel <mehdi.m_86@hotmail.fr>
2023-10-30 11:18:40 +0100

    Home+Footer+Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App font-amarante">
       <Navbar />
+      <Home />
       <Footer />
     </div>
   );
